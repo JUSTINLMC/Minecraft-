98c75e419149975e0bf402c30899abe28f5f4fad
JUSTINLMC <270418048@qq.com>
2023-01-27 10:20:56 +0800

    Update for library
    Updated from mojang-minecraft to @minecraft/server, etc.

--- gametest/myfirstgt.js
+++ gametest/myfirstgt.js
@@ -1,2 +1,14 @@
-import { world } from mojang-minecraft
+import {
+	DynamicPropertiesDefinition,
+	MinecraftEntityTypes,
+	world,
+  } from "@minecraft/server"
+  
+  world.events.worldInitialize.subscribe((e) => {
+	let def = new DynamicPropertiesDefinition();
 	
+	def.defineNumber("rpgStrength");
+	def.defineString("rpgRole", 16);
+	def.defineBoolean("rpgIsHero");
+	e.propertyRegistry.registerEntityTypeDynamicProperties(def, MinecraftEntityTypes.skeleton);
+  });
